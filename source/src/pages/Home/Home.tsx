33b32a3249ad86b5968b1cc71868227e7df93d65
unknown <seba.pinard@gmail.com>
2022-03-24 11:58:46 +0100

    remove scroll effect at home

--- source/src/pages/Home/Home.tsx
+++ source/src/pages/Home/Home.tsx
@@ -1,18 +1,18 @@
 import { WELCOM_HTML_TAG_ID } from "../../constants/site.constants";
 import JourneyBegins from "../../components/JourneyBegins/JourneyBegins";
-import { TSPasta } from "../../scripts/ts-pasta";
+// import { TSPasta } from "../../scripts/ts-pasta";
 import { NavLink } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/header";
 import { MirrorBG } from "../../components/MirrorBG/MirrorBG";
 
-const tsPasta = new TSPasta({ welcomeElementId: WELCOM_HTML_TAG_ID });
-const { onHeaderClick } = tsPasta;
+// const tsPasta = new TSPasta({ welcomeElementId: WELCOM_HTML_TAG_ID });
+// const { onHeaderClick } = tsPasta;
 
 const Home = () => {
   return (
     <>
-      <Header onClick={onHeaderClick} />
+      <Header />
       <JourneyBegins id={WELCOM_HTML_TAG_ID} />
       <NavLink to="piece">
         <Footer />
