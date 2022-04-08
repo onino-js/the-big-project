dd96e8d9627779e517193fa23a636a7c6a9b3a2a
unknown <seba.pinard@gmail.com>
2022-04-08 06:30:33 +0200

    deploy

--- source/src/pages/Gallery/Gallery.tsx
+++ source/src/pages/Gallery/Gallery.tsx
@@ -1,19 +1,35 @@
-import ImageGallery from "react-image-gallery";
+import styles from "./piece.module.scss";
+import classnames from "classnames/bind";
+// import Footer from "../../components/Footer/Footer";
+import { useSearchParams } from "react-router-dom";
+import { artPieceList } from "../../components/art-pieces/art-piece-index";
+import { ART_PIECE_PARAM } from "../../constants/site.constants";
+import GFY from "../../components/art-pieces/GFY/GFY";
+// import TSPasta from "../../scripts/ts-pasta";
+// import { NavLink } from "react-router-dom";
 
-const images = [
-  {
-    original: "https://picsum.photos/id/1018/1000/600/",
-    thumbnail: "https://picsum.photos/id/1018/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1015/1000/600/",
-    thumbnail: "https://picsum.photos/id/1015/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1019/1000/600/",
-    thumbnail: "https://picsum.photos/id/1019/250/150/",
-  },
-];
+const cx = classnames.bind(styles);
 
-export const Gallery = () => <ImageGallery items={images} />;
-export default Gallery;
+export const Piece: React.FC = () => {
+  const [searchParams] = useSearchParams();
+  const ArtPiece = artPieceList.find(
+    (ap) => ap.url === searchParams.get(ART_PIECE_PARAM)
+  );
+  let ArtPieceComponent = ArtPiece ? ArtPiece.component : GFY;
+
+  // const onFooterClick = () => {
+  //   const randomArtPiece = TSPasta.sample(artPieceList);
+  //   setSearchParams({ [ART_PIECE_PARAM]: randomArtPiece.url });
+  // };
+  return (
+    <>
+      <div className={cx("piece")}>
+        <ArtPieceComponent />
+        <br />
+        {/* <NavLink to="/inventory"></NavLink> */}
+        {/* <p>Or shuffle poetry by clicking below:</p> */}
+      </div>
+      {/* <Footer onClick={onFooterClick} /> */}
+    </>
+  );
+};
